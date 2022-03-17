cb69c3bb600bce968c9250ea1a8ce8146fe179a6
kuzudoli <enescakkr@windowslive.com>
2022-03-17 14:52:01 +0300

    Add authentication

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const session = require("express-session")
+const MongoStore = require('connect-mongo');
 const mongoose = require("mongoose");
+
 
 const pageRoute = require("./routes/pageRoute")
 const courseRoute = require("./routes/courseRoute")
 const categoryRoute = require("./routes/categoryRoute")
-const userRoute = require("./routes/userRoute")
+const userRoute = require("./routes/userRoute");
 
 mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
   console.log("DB Connect!");
@@ -15,10 +18,23 @@ const app = express();
 //Template Engine
 app.set('view engine', 'ejs');
 
+global.userIn = null;
+
 //Middlewares
 app.use(express.static("public"));
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({mongoUrl:'mongodb://localhost/smartedu-db'})
+}))
+
+app.use('*',(req,res,next)=>{
+  userIn = req.session.userID;
+  next();
+});
 
 app.use('/',pageRoute);
 app.use('/courses',courseRoute);
--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,7 @@ const User = require('../models/User');
 exports.createUser = async (req, res) => {
 	try {
 	    const user = await User.create(req.body);
-		res.status(201).json({
-			status: 'Success',
-			user
-		});
+		res.redirect("/login");
 	} catch(error){
 		res.status(400).json({
             status: "Failed",
@@ -24,7 +21,8 @@ exports.loginUser = async (req, res) => {
 			if(user){
 				bcrypt.compare(password, user.password,(err,same) => {
 			 		if(same){
-						res.status(200).send("SELAM");
+						req.session.userID = user._id;
+						res.redirect("/users/dashboard");
 			 		}
 				});
 		  	}
@@ -36,3 +34,17 @@ exports.loginUser = async (req, res) => {
         })
 	}
 };
+
+exports.logoutUser = async (req, res) => {
+	req.session.destroy(() => {
+	  res.redirect("/");
+	})
+};
+
+exports.getDashboard = async(req,res) => {
+	const user = await User.findOne({_id:req.session.userID})
+	res.status(200).render("dashboard",{
+		page_name:"dashboard",
+		user
+	});
+}
