2499f3dce3805350388812c40ea43bc4da1091de
kuzudoli <enescakkr@windowslive.com>
2022-03-21 11:18:53 +0300

    Add course-teacher relationship

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require('../models/User');
 const Category = require('../models/Category');
+const Course = require("../models/Course");
 
 exports.createUser = async (req, res) => {
 	try {
@@ -45,11 +46,13 @@ exports.logoutUser = async (req, res) => {
 exports.getDashboard = async(req,res) => {
 	const user = await User.findOne({_id:req.session.userID})
 	const categories = await Category.find();
+	const courses = await Course.find({user:req.session.userID});
 	if(user){
 		res.status(200).render("dashboard",{
 			page_name:"dashboard",
 			user,
-			categories
+			categories,
+			courses
 		});
 	}else{
 		res.redirect("/");
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,9 +1,15 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
+const User = require('../models/User');
 
 exports.createCourse = async (req, res) => {
 	try {
-	    const course = await Course.create(req.body);
+	    const course = await Course.create({
+			name:req.body.name,
+			description:req.body.description,
+			category:req.body.category,
+			user:req.session.userID
+		});
 		res.status(201).redirect("/courses");
 	} catch(error){
 		res.status(400).json({
@@ -42,7 +48,7 @@ exports.getAllCourses = async (req, res) => {
 exports.getCourse = async (req, res) => {
 	try {
         const courseSlug = req.params.slug; 
-	    const course = await Course.findOne({slug:courseSlug});
+	    const course = await Course.findOne({slug:courseSlug}).populate("user");
 		res.status(200).render("course",{
             course,
             page_name:'courses'
