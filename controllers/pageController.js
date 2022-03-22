5d37858c770d5994870866e0ab657d6898794c50
kuzudoli <enescakkr@windowslive.com>
2022-03-22 10:18:32 +0300

    Add send email service

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 exports.getIndexPage = (req,res) => {
     res.status(200).render("index",{
         page_name:"index"
@@ -26,4 +28,45 @@ exports.getContactPage = (req,res) => {
     res.status(200).render("contact",{
         page_name:"contact"
     });
+}
+
+exports.sendMail = async(req,res) => {
+    console.log(req.body);
+    const outputMessage  = `
+    
+    <h1>Mail Details:</h1>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.comment}</p>
+    `
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: 'pilavlivar5@gmail.com', // generated ethereal user
+            pass: 'pqavjuyvpnilrpwz', // generated ethereal password
+        },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <pilavlivar5@gmail.com>', // sender address
+        to: "enescakkr@windowslive.com", // list of receivers
+        subject: "Test Mail", // Subject line
+        html: outputMessage, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    res.status(200).redirect("/contact")
 }
