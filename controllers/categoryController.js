d79c0110a5f078857190d9caee0061f134d851dc
kuzudoli <enescakkr@windowslive.com>
2022-03-23 13:59:29 +0300

    Add create-delete category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,10 +3,7 @@ const Category = require('../models/Category');
 exports.createCategory = async (req, res) => {
 	try {
 	    const category = await Category.create(req.body);
-		res.status(201).json({
-			status: 'Success',
-			category
-		});
+		res.status(201).redirect("/users/dashboard");
 	} catch(error){
 		res.status(400).json({
             status: "Failed",
@@ -38,6 +35,19 @@ exports.getCourse = async (req, res) => {
             course,
             page_name:'courses'
         });
+	} catch(error){
+		res.status(400).json({
+            status: "Failed",
+            error
+        })
+	}
+};
+
+exports.deleteCategory = async (req, res) => {
+	try {
+        await Category.findByIdAndRemove(req.params.id);
+		//await Course.deleteMany({category:req.params.id});//Removes deleted user's courses if it is teacher
+		res.status(200).redirect("/users/dashboard");
 	} catch(error){
 		res.status(400).json({
             status: "Failed",
